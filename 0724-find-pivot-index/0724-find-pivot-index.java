1a8b9b7ae0410a4efbaffa239475edc7af17084c
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-04-13 18:46:35 +0530

    Time: 1 ms (96.68%), Space: 45.4 MB (45.32%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int rsum=0,lsum=0;
+        
+        for(int i=0;i<nums.length;i++){
+            rsum+=nums[i];
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            rsum=rsum-nums[i];
+            if(lsum==rsum){
+                return i;
+            }
+            lsum+=nums[i];
+        }
+        
+        return -1;
+    }
+}

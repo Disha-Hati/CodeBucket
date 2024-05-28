8ba80e80aba9cab43a606efd1e24b69454d8d4bf
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-05-28 18:29:33 +0530

    Time: 0 ms (100.00%), Space: 45 MB (63.10%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,26 +1,13 @@
 class Solution {
-    public void swap(int[] arr,int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    
     public int missingNumber(int[] nums) {
-        int i=0;
-        while(i<nums.length){
-            int correct=nums[i];
-            if(nums[i]<nums.length && nums[i]!=nums[correct]){
-                swap(nums,i,correct);
-            }else{
-                i++;
-            }
+        int n=nums.length;
+        int expSum=n*(n+1)/2;
+        
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
         
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=j){
-                return j;
-            }
-        }
-        return nums.length;
+        return expSum-sum;
     }
 }

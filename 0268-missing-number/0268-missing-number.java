7b6439edcc93f34d2d72d6453fe0138d4aca981c
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-05-28 18:48:31 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (48.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n=nums.length;
-        int expSum=n*(n+1)/2;
+        int xor1=0;
+        int xor2=0;
         
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+        for(int i=0;i<nums.length;i++){
+            xor1=xor1^(i);
+            xor2=xor2^nums[i];
         }
+        xor1=xor1^nums.length;
         
-        return expSum-sum;
+        return xor1^xor2;
     }
 }

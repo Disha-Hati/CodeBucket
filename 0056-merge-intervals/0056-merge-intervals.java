79607b59238150f8cebc8e201f9542fb3ba8003e
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-06-09 23:59:33 +0530

    Time: 8 ms (86.23%), Space: 46.6 MB (32.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> res=new ArrayList<>();
+        List<int[]> res=new ArrayList<>(); //List of Array
         
         if(intervals.length==0||intervals==null){
             return res.toArray(new int[0][]);
         }
+        
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         
         int start=intervals[0][0];
@@ -14,12 +15,14 @@ class Solution {
             if(i[0]<=end){
                 end=Math.max(end,i[1]);
             }else{
-                res.add(new int[]{start,end});
+                res.add(new int[] {start,end});
                 start=i[0];
                 end=i[1];
             }
         }
+        
         res.add(new int[]{start,end});
-        return res.toArray(new int[0][0]);
+        return res.toArray(new int[0][]);
+        
     }
 }

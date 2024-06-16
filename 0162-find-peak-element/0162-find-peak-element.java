8a62d990e7076e0166d4055284c424f69b7d0451
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-06-16 20:37:22 +0530

    Time: 0 ms (100.00%), Space: 42 MB (73.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,10 +6,10 @@ class Solution {
         while(start<end){
             int mid=start+(end-start)/2;
             
-            if(nums[mid]<nums[mid+1]){
-                start=mid+1;
-            }else{
+            if(nums[mid]>nums[mid+1]){
                 end=mid;
+            }else{
+                start=mid+1;
             }
         }
         

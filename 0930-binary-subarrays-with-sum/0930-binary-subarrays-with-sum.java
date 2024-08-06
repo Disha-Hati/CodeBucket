4d0c33fa605597e90d18c35375be7c21a5b801b8
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-08-07 01:16:31 +0530

    Time: 1 ms (100.00%), Space: 48 MB (37.29%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,16 +1,27 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
+    
+        return SubarraySumatmostK(nums,goal)-SubarraySumatmostK(nums,goal-1);
+    }
+    public int SubarraySumatmostK(int[] nums,int k){
+        if(k<0)  return 0;
+        int l=0,r=0;
+        
         int count=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                
-                if(sum==goal)  count++;
-                
-                if(sum>goal) break;
+        int sum=0;
+        
+        while(r<nums.length){
+            sum+=nums[r];
+            
+            while(sum>k){
+                sum-=nums[l];
+                l++;
             }
+            
+            count+=r-l+1;
+            r++;
         }
+        
         
         return count;
     }

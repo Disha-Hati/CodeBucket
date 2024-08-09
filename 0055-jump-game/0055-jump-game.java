360f8af39124fe18676553f8613aa77fe29742fe
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-08-10 01:47:16 +0530

    Time: 3 ms (51.74%), Space: 46 MB (8.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean canJump(int[] nums) {
         int maxIndex=0;
         
-        if(nums.length==1) return true;
         
         for(int i=0;i<nums.length;i++){
             if(i>maxIndex)  return false;

a64309db52ef182967131eaad4bb827b36001d80
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-10-19 20:59:27 +0530

    Time: 2 ms (85.90%), Space: 46.3 MB (6.30%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,29 +1,15 @@
 class Solution {
-    public void swap(int[] nums,int x,int y){
-        int temp=nums[x];
-        nums[x]=nums[y];
-        nums[y]=temp;
-    }
     public void moveZeroes(int[] nums) {
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0) { //finding the first non zero number
-                j=i;
-                break;
+        int i=0; //to track 
+        
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                nums[i++]=nums[j];
             }
         }
-        if(nums[j]==0) return;   //It has all zeroes
         
-        swap(nums,0,j); 
-        
-        int i=0;
-        j=1;
-        while(j<nums.length){
-            if(nums[j]!=0){
-                swap(nums,i+1,j);
-                i++;
-            }
-            j++;
+        while(i<nums.length){
+            nums[i++]=0;
         }
     }
 }

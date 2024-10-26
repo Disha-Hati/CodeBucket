3f286a84912206444c0ebe5854a8a148a7ae6ab2
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-10-26 11:30:26 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (38.93%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,48 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int p= -1;
-        //scanning from right to left,finding element less than its previous
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                p = i - 1;//p=lesser one index
+        //find the breaking point
+        int breakIndex=-1;
+        
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                breakIndex=i;
                 break;
             }
         }
         
-        //last order where we need to return lowest
-        if (p == -1) {
-            reverse(nums, 0, nums.length - 1);
+        if(breakIndex==-1){
+            reverse(nums,0,nums.length-1); //The given array is the last permutation
             return;
-        }
- 
-        //from right finding element greater than p
-        int q= nums.length-1;
-        for (int i = nums.length-1; i >= p+1; i--) {
-            if (nums[i] > nums[p]) {
-                q = i;
+        }  
+        
+        for(int i=nums.length-1;i>breakIndex;i--){
+            if(nums[i]>nums[breakIndex]){
+                swap(nums,i,breakIndex);
                 break;
-             }
+            }
         }
         
-        //int q=p+1;
-        
-        //swap p and q
-        swap(nums, p, q);
-        
-        reverse(nums, p+1 , nums.length - 1);
+        reverse(nums,breakIndex+1,nums.length-1);
     }
     
-     private void swap(int[] nums,int p,int q){
-        int temp=nums[p];
-        nums[p]=nums[q];
-        nums[q]=temp;
+    public void swap(int[] nums,int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
     
-    private void reverse(int[] nums, int i, int j) {
-    while (i < j) {
-        swap(nums, i, j);
-        i++;
-        j--;
-        }   
+    public void reverse(int[] nums,int s,int e){
+        while(s<=e){
+            int temp=nums[s];
+            nums[s]=nums[e];
+            nums[e]=temp;
+            s++;
+            e--;
+        }
     }
 }

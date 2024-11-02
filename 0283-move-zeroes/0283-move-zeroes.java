47a51e2770549a45e5d49935ee459bb8739985c5
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-11-02 21:59:44 +0530

    Time: 2 ms (84.72%), Space: 45.8 MB (49.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,28 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0; //to track 
+        int index=-1;
         
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                nums[i++]=nums[j];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                index=i;
+                break;
             }
         }
         
-        while(i<nums.length){
-            nums[i++]=0;
+        if(index==-1) return; //no zeroes
+        
+        int j=index;
+        int k=j+1;
+        
+        while(k<nums.length){
+            if(nums[k]!=0){
+                int temp=nums[k];
+                nums[k]=nums[j];
+                nums[j]=temp;
+                
+                j++;
+            }
+            k++;
         }
     }
 }

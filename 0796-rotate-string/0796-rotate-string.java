9f0c5bb56083b6eeba142fc7010cff5be2c371f0
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-11-04 00:32:29 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (20.30%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        
+        if (s.length() != goal.length()) {
+        return false;
+        }
+        
+       String doubledS = s + s;
+    return doubledS.contains(goal);
+    }
+}

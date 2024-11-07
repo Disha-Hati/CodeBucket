7f11d0a35eb4357ae2e7d384ae4d8bed852b2c4b
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-11-07 20:54:06 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (68.39%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,21 +2,23 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list=new ArrayList<>();
         List<Integer> ans=new ArrayList<>();
-        callRec(0,nums,list,ans);
+        
+        int n=nums.length;
+        callFunc(0,ans,nums,n,list);
         
         return list;
     }
     
-    public void callRec(int index,int[] arr,List<List<Integer>> list,List<Integer> ans){
-         
-        if(index==arr.length){
+    public void callFunc(int index,List<Integer> ans,int[]nums,int n,List<List<Integer>> list){
+        if(index==n){
             list.add(new ArrayList<>(ans));
             return;
         }
         
-        ans.add(arr[index]);
-        callRec(index+1,arr,list,ans);
+        ans.add(nums[index]);
+        callFunc(index+1,ans,nums,n,list);
         ans.remove(ans.size() - 1);
-        callRec(index+1,arr,list,ans);
+        callFunc(index+1,ans,nums,n,list);
+        
     }
 }

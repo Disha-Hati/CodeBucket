284857af816ac22ba642ef969a8923ce28af641e
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-11-13 11:57:35 +0530

    Time: 2 ms (43.90%), Space: 44.5 MB (34.48%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-   List<List<Integer>> list = new ArrayList<>();
-   // Arrays.sort(nums); // not necessary
-   backtrack(list, new ArrayList<>(), nums);
-   return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-   if(tempList.size() == nums.length){
-      list.add(new ArrayList<>(tempList));
-       return;
-   }
-      for(int i = 0; i < nums.length; i++){ 
-         if(tempList.contains(nums[i])) continue; // element already exists, skip
-         tempList.add(nums[i]);
-         backtrack(list, tempList, nums);
-         tempList.remove(tempList.size() - 1);
-      }
-   
-} 
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        
+        backtrack(nums,ans,curr);
+        return ans;
+    }
+    
+    public void backtrack(int[] nums,List<List<Integer>> ans,List<Integer> curr){
+        if(curr.size()==nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(curr.contains(nums[i])) continue;
+            curr.add(nums[i]);
+            backtrack(nums,ans,curr);
+            curr.remove(curr.size()-1);
+        }
+    }
 }

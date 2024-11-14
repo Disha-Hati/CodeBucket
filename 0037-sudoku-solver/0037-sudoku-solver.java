796c7d5f120813717f9ac69ae8de16c7fce4ee29
Disha Hati <70029959+Disha-Hati@users.noreply.github.com>
2024-11-14 14:04:17 +0530

    Time: 11 ms (33.04%), Space: 41 MB (58.28%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,45 +1,41 @@
 class Solution {
-    
-    public boolean helper(char[][] board){
-        
+    public boolean solve(char[][] board){
         for(int row=0;row<board.length;row++){
             for(int col=0;col<board.length;col++){
-                if(board[row][col]=='.'){  //empty
-                    for(char val = '1'; val <= '9'; val++){
-                        if(isSafe(board,row,col,val)){
-                            board[row][col] = val;
+                if(board[row][col]=='.'){
+                    for(char val='1';val<='9';val++){
+                        if(isValid(board,row,col,val)){
+                            board[row][col]=val;
                             
-                            if(helper(board)){
-                                return true;
-                            }else{  //backtrack
-                               board[row][col] = '.';
-                            }
+                            if(solve(board)) return true;
+                            else  board[row][col] = '.';
                         }
                     }
-                    return false;
+                     return false;
                 }
             }
+            
         }
         return true;
     }
     
-    public boolean isSafe(char[][] board,int row,int col,char val){
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    public boolean isValid(char[][] board,int row,int col,char val){
+        
         for(int i=0;i<board.length;i++){
-            //row
-            if(board[row][i]==val) return false;
-            
-            //col
+            //vertical
             if(board[i][col]==val)  return false;
+            
+            //horizontal
+            if(board[row][i]==val) return false;            
             
             //grid
             if(board[row/3*3 + i/3][col/3*3 + i%3] == val) return false;
         }
         
         return true;
-    }
-    
-    
-    public void solveSudoku(char[][] board) {
-        helper(board);
     }
 }
